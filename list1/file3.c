eb3265671869701131a01b1c69712952122b6b15
SanketPatil29 <sanketp0023@gmail.com>
2023-08-24 15:15:08 +0530

    File3 uploaded

---
+++ list1/file3.c
@@ -0,0 +1,13 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdio.h>
+
+int main()
+{
+	int fd = creat("a.txt", O_RDONLY | O_WRONLY | O_RDWR);
+	printf("fd = %d\n", fd);
+	int fd1 = creat("b.txt", O_RDONLY | O_WRONLY | O_RDWR);
+	printf("fd1 = %d\n", fd1);
+	return 0;
+}
