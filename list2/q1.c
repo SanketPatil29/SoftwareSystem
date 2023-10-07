42c0b0e61aef58b90bd0148620c42ecfbc5f611e
SanketPatil29 <sanketp0023@gmail.com>
2023-10-07 18:35:15 +0530

    A

---
+++ list2/q1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+int main()
+{
+	int fd[2], n;
+	char buffer[100];
+	pid_t p;
+	pipe(fd);
+	p = fork();
+	if(p>0)
+	{
+		write(fd[1], "hello\n", 6);
+	}
+	else
+	{
+	}
---
+++ list2/temp.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("Helllooo");
+    return 0;
+}
